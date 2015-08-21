22d34ee7f232051c3ee6b2189b04e089c2fc9f30
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-08-20 17:53:26 -0700

    Updated out of date libraries

--- src/collections/edgecollection.js
+++ src/collections/edgecollection.js
@@ -4,19 +4,19 @@ var _ = require('lodash');
 
 var EdgeCollection = Backbone.Collection.extend( {
 
-    model: Edge,
-    url: '/foo/bar',
+  model: Edge,
+  url: '/foo/bar',
 
-    initialize(collection, graph) {
-      this.graph = graph;
-    },
+  initialize(collection, graph) {
+    this.graph = graph;
+  },
 
-    toCytoscape() {
-      var edges = _.map(this.models, function(d) {
-        return d.toCytoscape();
-      });
-      return edges;
-    }
+  toCytoscape() {
+    var edges = _.map(this.models, function(d) {
+      return d.toCytoscape();
+    });
+    return edges;
+  }
 });
 
 module.exports = EdgeCollection;
